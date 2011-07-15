c7d1086f91ebadac6cae817cb9fc4490e1b4effc
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-07-14 22:48:18 -0600

    Example w/Interactive Keyboard input

--- src/main/java/com/memetix/chits4jexamples/Chit4jHelloWorld.java
+++ src/main/java/com/memetix/chits4jexamples/Chit4jHelloWorld.java
@@ -2,6 +2,7 @@ package com.memetix.chits4jexamples;
 
 import com.memetix.chits4j.metric.Metric;
 import com.memetix.chits4j.metric.MetricService;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -12,17 +13,38 @@ public class Chit4jHelloWorld
     public static void main( String[] args )
     {
         MetricService service = new MetricService();
-        Metric metric = new Metric();
-        metric.setAgent("Example Agent");
-        metric.setName("Ran Example");
-        metric.setValue(""+System.currentTimeMillis());
         
-        try {
-            service.writeMetric(metric);
-            System.out.println(metric.encodeAsJSON().toString());
-            service.deleteMetric(metric.getId());
-        } catch(Exception e) {
-            System.out.println("Error writing Metric" + e.getMessage());
+        String value = "";
+        Scanner input=new Scanner(System.in); // Decl. & init. a Scanner.
+        input.useDelimiter("\\n");
+  
+        System.out.print("Metric Value > ");
+        while((value=input.next())!=null) {
+            if(value.equalsIgnoreCase("exit")||value.equalsIgnoreCase("quit")||value.equalsIgnoreCase("q"))
+                System.exit(0);
+            System.out.println();  
+
+            Metric metric = new Metric();
+            metric.setAgent("Example.User@gmail.com");
+            metric.setName("Recorded Text");
+            metric.setValue(value);
+            
+            Metric wordCountMetric = new Metric();
+            wordCountMetric.setAgent("Example.User@gmail.com");
+            wordCountMetric.setName("Character Count");
+            wordCountMetric.setValue("" + value.length());
+
+            try {
+                service.writeMetric(metric);
+                service.writeMetric(wordCountMetric);
+                System.out.println(metric.encodeAsJSON().toString());
+                System.out.println(wordCountMetric.encodeAsJSON().toString());
+                service.deleteMetric(metric);
+                service.deleteMetric(wordCountMetric);
+            } catch(Exception e) {
+                System.out.println("Error writing Metric" + e.getMessage());
+            }
+            System.out.print("Metric Value > ");
         }
     }
 }
